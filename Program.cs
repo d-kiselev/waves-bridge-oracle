303ec8c36796f4a234934d7d3053301970d3f381
d-kiselev <dkiselev1234@gmail.com>
2019-12-27 13:28:38 +0300

    add project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wavesbridgeoracle
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
